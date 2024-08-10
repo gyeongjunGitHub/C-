9fb5a6e6f24081e6a115d31061e1e25c64a69ee9
JUN <q1sw23zx@naver.com>
2024-08-10 18:46:39 +0900

    변수 종류

---
+++ Project1/Project1/func.cpp
@@ -0,0 +1,15 @@
+
+//참조 해야함 func.h 의 구현체
+//#include "func.h" // -> 안적어도 ㄱㅊ
+#include"common.h"
+#include<stdio.h>
+int Add(int a, int b)
+{
+	printf("%d", shit);
+	return a + b;
+}
+
+int Sub(int a, int b) 
+{
+	return a - b;
+}
---
+++ Project1/Project1/func.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// 이런 함수가 존재한다. 선언
+// interface?
+int Add(int a, int b);
+
+int Sub(int a, int b);
