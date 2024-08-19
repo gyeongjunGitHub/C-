cd580dcccdcaffded9640fed975046408c21d93d
JUN <q1sw23zx@naver.com>
2024-08-19 18:06:45 +0900

    함수 템플릿, 클래스 템플릿 가변배열을 클래스 템플릿으로.

--- Project1/Project1/CArr.h
+++ Project1/Project1/CArr.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include<assert.h>
+
+// Ctrl + R + R 동일 이름 모두 바꾸기?
+template<typename T>
 class CArr
 {
 
 //필드
 //사실 defalut 접근자는 private이다.
 private:
-	int* m_pInt;
+	T* m_pData;
 	int m_iCount;
 	int m_iMaxCount;
 
@@ -18,7 +22,10 @@ public:
 	~CArr();
 
 	// pushBack 함수
-	void PushBack(int _Data);
+	// template으로 바꾼 시점부터 어떤 자료형이든 다 받을 수 있다.
+	// 크기가 큰 자료형을 받아오는 경우 pushBack 내부의 지역 변수로 값을 복사하여 배열에 저장하는 경우 비 효율적이다.
+	// 따라서 참조 변수나 주소값을 받아오는게 좋다.
+	void PushBack(const T& _Data);
 
 	// resize 함수
 	// 기존 C스타일로 만들었을 때와 다르게 resize함수를 단독으로 사용할 수 있게 바뀜
@@ -31,5 +38,97 @@ public:
 	// cArr[1] = 100; 도 가능하도록 int* 타입으로 선언
 	// 하지만 반환 타입이 주소값이여서 *cArr[1] = 100; 이렇게 적어야함
 	// 그래서 레퍼런스 변수 를 사용
-	int& operator[](int index);
+	T& operator[](int index);
+
+	int returnSize();
 };
+
+// new 키워드는 동적 할당
+// 1. CArr 자료형 크기 만큼 메모리 할당
+// 2. CArr 자료형 인걸 컴파일러가 인지하고 생성자를 통해 초기값 세팅까지
+//CArr* cTest = new CArr;
+
+// CArr 자료형을 확인하고 해당 클레스의 소멸자를 통해 삭제
+//delete cTest;
+
+
+// 중요사항
+// 클레스 템플릿을 만들 때
+// 템플릿의 함수 들은 cpp파일이 아니라 헤더 파일에 존재해야한다.
+
+
+// 스코프
+// 템플릿 생성자
+template<typename T>
+CArr<T>::CArr()
+// 멤버 변수들
+	:m_pData(nullptr)  //int* m_pInt;
+	, m_iCount(0)
+	, m_iMaxCount(2)
+{
+	// new 동적할당 키워드, C에서는 malloc
+	m_pData = new T[2]; // T 자료형 2개 크기 메모리 할당
+}
+
+
+template<typename T>
+CArr<T>::~CArr()
+{
+	// int 자료형 일 때는 delete
+	// int[] 자료형 일 때는 delete[]
+	// 메모리 해제
+	delete[] m_pData;
+}
+
+template<typename T>
+void CArr<T>::PushBack(const T& _Data)
+{
+	if (m_iCount >= m_iMaxCount)
+	{
+		//재할당
+		resize(m_iMaxCount * 2);
+	}
+
+	// 데이터 추가
+	m_pData[m_iCount++] = _Data;
+}
+
+template<typename T>
+void CArr<T>::resize(int _iResizeCount)
+{
+	// 늘리려는 사이즈가 maxCount 보다 작거나 같으면 예외 처리 (의미없음X)
+	if (m_iMaxCount >= _iResizeCount)
+	{
+		assert(nullptr);
+	}
+
+	// 1. resize size 만큼 동적 할당
+	T* pNew = new T[_iResizeCount];
+
+	// 2. 기존 데이터를 새로운 공간으로 복사
+	for (int i = 0; i < m_iCount; i++)
+	{
+		pNew[i] = m_pData[i];
+	}
+
+	// 3. 기존 공간 메모리 해제
+	delete[] m_pData;
+
+	// 4. 새로 할당된 공간 가르키기
+	m_pData = pNew;
+
+	// 5. maxCount 변경
+	m_iMaxCount = _iResizeCount;
+}
+
+template<typename T>
+T& CArr<T>::operator[](int index)
+{
+	return m_pData[index];
+}
+
+template<typename T>
+int CArr<T>::returnSize()
+{
+	return m_iCount;
+}
--- Project1/Project1/CArrListMain.cpp
+++ Project1/Project1/CArrListMain.cpp
@@ -1,17 +1,49 @@
 #include<stdio.h>
 #include"CArr.h"
 
+
+// 템플릿 
+// typeName 대신 class를 적어도 됨
+// 여러가지 자료형을 받을 수 있느 함수의 틀.
+// 만들어 놓고 호출하지 않으면 함수는 프로그램 내에 존재하지 않음
+// T는 자료형 자리
+template<typename T>
+T Add(T a, T b)
+{
+	return a + b;
+}
+
 int main()
 {
-	CArr cTest;
-	
-	for (int i = 0; i < 10; i++)
-	{
-		cTest.PushBack(i);
-	}
+	//CArr cTest;
 
-	cTest[9] = 123456;
-	printf("%d\n", cTest[9]);
+	//cTest[9] = 123456;
+	//printf("%d\n", cTest[9]);
+
+	// 함수 템플릿.
+	// Add는 함수가 아님. 함수 템플릿(틀)이다.
+	// 여러가지 자료형이 사용 가능한 함수 틀.
+	int i = Add<int>(10, 30);
+
+	// 클레스 템플릿 
+	// 자바 제네릭 이랑 비슷한 느낌인걸 
+	// List<String> myList = new ArrayList(); 요런느낌 String 타임을 저장한느 리스트이다.
+	// 타입이 정해진 버전을 요청해야 템플릿을 통해 클래스가 생성됨.
+
+	// int 타입 가변배열
+	CArr<int> carr;
+
+	// float 타입 가변배열
+	CArr<float> carrf;
+
+	carr.PushBack(10);
+	carr.PushBack(20);
+	carr.PushBack(30);
+
+	for (int i = 0; i < carr.returnSize(); i++)
+	{
+		printf("%d\n", carr[i]);
+	}
 
 	return 0;
 }
